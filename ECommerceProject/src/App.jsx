103a54909b726930dddf703484177121994c8f46
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-09-06 14:53:03 +0530

    3rd commit

--- ECommerceProject/src/App.jsx
+++ ECommerceProject/src/App.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-import Login from './components/Login.jsx';
-import ViewCategories from './components/ViewCategories.jsx';
-import ViewProducts from './components/ViewProducts.jsx';
-import ViewProductDetails from './components/ViewProductDetails.jsx';
+import Login from './pages/Login';
+import ViewCategories from './pages/ViewCategories';
+import ViewProducts from './pages/ViewProducts';
+import ViewProductDetails from './pages/ViewProductDetails';
+import ViewCart from './pages/ViewCart';
+// import ViewCheckout from './pages/ViewCheckout'
 import { useHistory } from "react-router-dom";
 import { useCookies } from 'react-cookie';
+import './index.css';
 
 const Header = () =>{
   const history = useHistory();
   const [cookies, setCookie, removeCookie] = useCookies(['PMartSecrete']);
   return(
-    <nav className="navbar navbar-expand-md bg-dark navbar-dark ">
+    <nav className="navbar navbar-expand-lg bg-custom-2">
       <div className="navbar-header">
-        <Link className="navbar-brand" to="/">PMart</Link>
+        <Link className="navbar-brand font-styling" to="/">PMart</Link>
       </div>
       <ul className="navbar-nav mr-auto">  
         <li className="nav-item">
-            <Link className="nav-link" to="/MyCart">My Cart</Link>
+            <Link className="nav-link font-styling" to="/mycart">My Cart</Link>
           </li>
         </ul>
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link className="nav-link" onClick={()=> {
+            <Link className="nav-link font-styling" onClick={()=> {
               removeCookie("Token");
               history.push('/login')
             } }>Logout</Link>
@@ -40,8 +43,10 @@ const App = () => {
           <Header />
           <Switch>
             <Route exact path="/" component={ViewCategories}/>
-            <Route path="/products/:categoryid" component={ViewProducts}/>
-            <Route path="/productdetails/:productid" component={ViewProductDetails}/>
+            <Route path="/products/:categoryId" component={ViewProducts}/>
+            <Route path="/productdetails/:productId" component={ViewProductDetails}/>
+            <Route path="/mycart" component={ViewCart}/>
+            {/* <Route path="/checkout" component={ViewCheckout}/> */}
             <Route path="/login" component={Login} />
           </Switch>
         </div>
--- ECommerceProjectAPI/src/routes/routing.js
+++ ECommerceProjectAPI/src/routes/routing.js
@@ -6,10 +6,13 @@ routing.post("/login", async(req, res, next) => {
     try {
         const { email, password } = req.body;
         if (email && password) {
-            db.all(`SELECT * FROM customer_table WHERE Email='${email}' AND Password='${password}'`, (err, row) => {
+            db.all(`SELECT * FROM customer_table WHERE email='${email}' AND password='${password}'`, (err, row) => {
                 if (err) res.send({ status: 'failed', msg: err.message })
                 if (row.length > 0) {
-                    res.send({ status: 'success', msg: 'Login Successfully' })
+                    final_res = {}
+                    final_res.customer_id = row[0].customer_id;
+                    final_res.name = row[0].name;
+                    res.send({ status: 'success', msg: 'Login Successfully', res: final_res })
                 } else {
                     res.send({ status: 'failed', msg: 'Authentication Failed' })
                 }
@@ -42,9 +45,9 @@ routing.get("/categories", async(req, res, next) => {
 
 routing.post("/products", async(req, res, next) => {
     try {
-        const { CategoryID } = req.body;
-        if (CategoryID) {
-            db.all(`SELECT * FROM product_table WHERE CategoryID='${CategoryID}' and Isavailable=1`, (err, row) => {
+        const { categoryId } = req.body;
+        if (categoryId) {
+            db.all(`SELECT * FROM product_table WHERE category_id='${categoryId}' and is_available=1`, (err, row) => {
                 if (err) {
                     res.send({ status: 'failed', msg: err.message })
                 }
@@ -64,9 +67,18 @@ routing.post("/products", async(req, res, next) => {
 
 routing.post("/productdetails", async(req, res, next) => {
     try {
-        const { ProductId } = req.body;
-        if (ProductId) {
-            db.all(`SELECT * FROM product_table WHERE ProdID='${ProductId}'`, (err, row) => {
+        const { productId, customerId } = req.body;
+        if (productId) {
+            db.all(`
+                SELECT 
+                pt.*, 
+                (
+                    SELECT ct.cart_id 
+                    FROM cart_table ct 
+                    WHERE ct.product_id='${productId}' AND ct.customer_id='${customerId}'
+                ) as cart_id 
+                FROM product_table pt 
+                WHERE pt.product_id='${productId}'`, (err, row) => {
                 if (err) {
                     res.send({ status: 'failed', msg: err.message })
                 }
@@ -84,5 +96,115 @@ routing.post("/productdetails", async(req, res, next) => {
     }
 })
 
+routing.post("/addcart", async(req, res, next) => {
+    try {
+        const { productId, customerId } = req.body;
+        if (productId && customerId) {
+            db.all(`INSERT INTO cart_table (product_id, customer_id, quantity)
+             VALUES('${productId}', '${customerId}', '1')`, (err, row) => {
+                if (err) {
+                    res.send({ status: 'failed', msg: err.message })
+                }
+                if (!err) {
+                    res.send({ status: 'success', msg: "Product added into cart" })
+                } else {
+                    res.send({ status: 'failed', msg: "Can not Add into the cart" })
+                }
+            })
+        } else {
+            res.send({ status: 'failed', msg: 'Could not get product ID or customer ID' })
+        }
+    } catch (err) {
+        res.send({ status: 'failed', msg: 'Something went wrong' })
+    }
+})
+
+routing.post("/mycart", async(req, res, next) => {
+    try {
+        const { customerId } = req.body;
+        if (customerId) {
+            db.all(`SELECT pt.*, ct.cart_id, ct.quantity FROM cart_table as ct INNER Join product_table pt on ct.product_id=pt.product_id WHERE ct.customer_id ='${customerId}'`, (err, row) => {
+                if (err) {
+                    res.send({ status: 'failed', msg: err.message })
+                }
+                if (row.length > 0) {
+                    res.send({ status: 'success', res: row })
+                } else {
+                    res.send({ status: 'failed', msg: "Cart Empty" })
+                }
+            })
+        } else {
+            res.send({ status: 'failed', msg: 'Could not get customer ID' })
+        }
+    } catch (err) {
+        res.send({ status: 'failed', msg: 'Something went wrong' })
+    }
+})
+
+routing.post("/removecartitem", async(req, res, next) => {
+    try {
+        const { cartId } = req.body;
+        if (cartId) {
+            db.all(`DELETE FROM cart_table WHERE cart_id='${cartId}'`, (err, row) => {
+                if (err) {
+                    res.send({ status: 'failed', msg: err.message })
+                }
+                if (!err) {
+                    res.send({ status: 'success', msg: "Removed Product from Cart" })
+                } else {
+                    res.send({ status: 'failed', msg: "Can not Remove item from the cart" })
+                }
+            })
+        } else {
+            res.send({ status: 'failed', msg: 'Could not get cart ID' })
+        }
+    } catch (err) {
+        res.send({ status: 'failed', msg: 'Something went wrong' })
+    }
+})
+
+routing.post("/decrementcartitem", async(req, res, next) => {
+    try {
+        const { cartId } = req.body;
+        if (cartId) {
+            db.all(`UPDATE cart_table SET quantity=quantity-1 WHERE cart_id=${cartId}`, (err, row) => {
+                if (err) {
+                    res.send({ status: 'failed', msg: err.message })
+                }
+                if (!err) {
+                    res.send({ status: 'success', msg: "Quantity Updated" })
+                } else {
+                    res.send({ status: 'failed', msg: "Can not update cart" })
+                }
+            })
+        } else {
+            res.send({ status: 'failed', msg: 'Could not get cart ID' })
+        }
+    } catch (err) {
+        res.send({ status: 'failed', msg: 'Something went wrong' })
+    }
+})
+
+routing.post("/incrementcartitem", async(req, res, next) => {
+    try {
+        const { cartId } = req.body;
+        if (cartId) {
+            db.all(`UPDATE cart_table SET quantity=quantity+1 WHERE cart_id=${cartId}`, (err, row) => {
+                if (err) {
+                    res.send({ status: 'failed', msg: err.message })
+                }
+                if (!err) {
+                    res.send({ status: 'success', msg: "Quantity Updated" })
+                } else {
+                    res.send({ status: 'failed', msg: "Can not update cart" })
+                }
+            })
+        } else {
+            res.send({ status: 'failed', msg: 'Could not get cart ID' })
+        }
+    } catch (err) {
+        res.send({ status: 'failed', msg: 'Something went wrong' })
+    }
+})
 
 module.exports = routing;
